b0060adcba93c1979cbe83aaa481df7f4d498db1
RafaelPerez de Lima <dr.rafalima@hotmail.com>
2020-12-14 05:27:22 -0300

    Criação da controller produto

--- QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -7,39 +7,42 @@ namespace QuickBuy.Repositorio.Repositorios
 {
     public class BaseRepositorio<TEntity> : IBaseRepositorio<TEntity> where TEntity : class
     {
-        private readonly QuickBuyContexto _quickBuyContexto;
+        protected readonly QuickBuyContexto QuickBuyContexto;
         public BaseRepositorio(QuickBuyContexto quickBuyContexto)
         {
-            _quickBuyContexto = quickBuyContexto;
+            QuickBuyContexto = quickBuyContexto;
         }
         public void Adicionar(TEntity entity)
         {
-            _quickBuyContexto.Set<TEntity>().Add(entity);
+            QuickBuyContexto.Set<TEntity>().Add(entity);
+            QuickBuyContexto.SaveChanges();
         }
 
         public void Atualizar(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Set<TEntity>().Update(entity);
+            QuickBuyContexto.SaveChanges();
         }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Dispose();
         }
 
         public TEntity ObterPorId(int id)
         {
-            throw new System.NotImplementedException();
+            return QuickBuyContexto.Set<TEntity>().Find(id);
         }
 
         public IEnumerable<TEntity> ObterTodos()
         {
-            return _quickBuyContexto.Set<TEntity>().ToList();
+            return QuickBuyContexto.Set<TEntity>().ToList();
         }
 
         public void Remover(TEntity entity)
         {
-            throw new System.NotImplementedException();
+            QuickBuyContexto.Remove(entity);
+            QuickBuyContexto.SaveChanges();
         }
     }
 }
